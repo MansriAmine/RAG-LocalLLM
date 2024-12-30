d5dd0e16164e73763b887d628fed03632e7ed8f4
Amine Mansri <mansrimedamin@gmail.com>
2024-12-30 19:55:09 +0100

    ADD TOKEN caulculator

---
+++ token_calculator.py
@@ -0,0 +1,84 @@
+from mistral_common.protocol.instruct.messages import (
+    AssistantMessage,
+    UserMessage,
+    ToolMessage
+)
+from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+from mistral_common.protocol.instruct.tool_calls import Function, Tool, ToolCall, FunctionCall
+from mistral_common.protocol.instruct.request import ChatCompletionRequest
+
+
+tokenizer_v3 = MistralTokenizer.v3()
+
+
+
+tokenized = tokenizer_v3.encode_chat_completion(
+    ChatCompletionRequest(
+        tools=[
+            Tool(
+                function=Function(
+                    name="get_current_weather",
+                    description="Get the current weather",
+                    parameters={
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",
+                            },
+                            "format": {
+                                "type": "string",
+                                "enum": ["celsius", "fahrenheit"],
+                                "description": "The temperature unit to use. Infer this from the users location.",
+                            },
+                        },
+                        "required": ["location", "format"],
+                    },
+                )
+            )
+        ],
+        messages=[
+            UserMessage(content="What's the weather like today in Paris"),
+            AssistantMessage(
+                content=None,
+                tool_calls=[
+                    ToolCall(
+                        id="VvvODy9mT",
+                        function=FunctionCall(
+                            name="get_current_weather",
+                            arguments='{"location": "Paris, France", "format": "celsius"}',
+                        ),
+                    )
+                ],
+            ),
+            ToolMessage(
+                tool_call_id="VvvODy9mT", name="get_current_weather", content="22"
+            ),
+            AssistantMessage(
+                content="The current temperature in Paris, France is 22 degrees Celsius.",
+            ),
+            UserMessage(content="What's the weather like today in San Francisco"),
+            AssistantMessage(
+                content=None,
+                tool_calls=[
+                    ToolCall(
+                        id="fAnpW3TEV",
+                        function=FunctionCall(
+                            name="get_current_weather",
+                            arguments='{"location": "San Francisco", "format": "celsius"}',
+                        ),
+                    )
+                ],
+            ),
+            ToolMessage(
+                tool_call_id="fAnpW3TEV", name="get_current_weather", content="20"
+            ),
+        ],
+        model="test",
+    )
+)
+
+tokens, text = tokenized.tokens, tokenized.text
+
+print(tokens)
+print(text)
